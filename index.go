ea9d712f4a722e319c31ceca564eac572b793e34
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-17 21:56:10 -0300

    list

---
+++ index.go
@@ -0,0 +1,101 @@
+package list
+
+import (
+	"github.com/intdxdt/math"
+	"github.com/intdxdt/deque"
+)
+
+const N = 32
+
+//List - Type
+type List struct {
+	*deque.Deque
+}
+
+//NewList - constructor the list type
+func NewList(initSize ...int) *List {
+	var iSize = N
+	if len(initSize) > 0 {
+		iSize = math.MaxInt(1, initSize[0])
+	}
+	return &List{deque.NewDeque(iSize)}
+}
+
+//Drop - drops entire list - similar to empty the list
+func (lst *List) Clear() {
+	lst.Deque.Clear()
+}
+
+//Len - computes the length of list
+func (lst *List) Len() int {
+	return lst.Deque.Len()
+}
+
+//First - gets first item in the list
+func (lst *List) First() interface{} {
+	if lst.Len() > 0 {
+		return lst.Get(0)
+	}
+	return nil
+}
+
+//Last - gets last item in the list
+func (lst *List) Last() interface{} {
+	if lst.Len() > 0 {
+		return lst.Get(-1)
+	}
+	return nil
+}
+
+//Append - adds to end of list
+func (lst *List) Append(data interface{}) *List {
+	lst.Deque.Append(data)
+	return lst
+}
+
+//AppendLeft - adds item at start of the list
+func (lst *List) AppendLeft(data interface{}) *List {
+	lst.Deque.AppendLeft(data)
+	return lst
+}
+
+//Pop - removes item from the last of the list
+func (lst *List) Pop() interface{} {
+	if lst.Len() == 0 {
+		return nil
+	}
+	return lst.Deque.Pop()
+}
+
+//PopLeft - removes item from front of list
+func (lst *List) PopLeft() interface{} {
+	if lst.Len() == 0 {
+		return nil
+	}
+	return lst.Deque.PopLeft()
+}
+
+//At finds the item at given at index n - O(1)
+//Supports negative indexing
+func (lst *List) At(n int) interface{} {
+	if n < 0 {
+		n = lst.Len() + n
+	}
+	if n >= 0 && n < lst.Len() {
+		return lst.Deque.Get(n)
+	}
+	return nil
+}
+
+//Each - iterates over items in the list
+func (lst *List) Each(fn func(interface{}, int) bool) {
+	lst.ForEach(func(v interface{}, index int) bool {
+		fn(v, index)
+		return true
+	})
+}
+
+//Stringer method
+func (lst *List) String() string {
+	return lst.Deque.String()
+}
---
+++ list_test.go
@@ -0,0 +1,100 @@
+package list
+
+import (
+	"testing"
+	"fmt"
+	"github.com/franela/goblin"
+)
+
+var array = []float64{0, 1, 0, 2, 3, 4, 3, 3.3, 9, 29, 3.1, 0.1,
+                      1.1, 1.81, 0.91, 0.81, 0.71, 0.88, 0.82, 0.81,
+}
+
+func TestSSet(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	g.Describe("List", func() {
+
+		g.It("should test list of items", func() {
+
+			var lst = NewList()
+			lst.Append("foo")
+			//head and tail should be the same
+			g.Assert(lst.First()).Eql(lst.Last())
+			lst.Append("bar")
+			lst.Append("baz")
+			lst.Append("boo")
+			lst.Append("asd")
+			lst.Append("dsa")
+			lst.Append("elf")
+			lst.Append("fro")
+			lst.Append("gap")
+			lst.Append("hoo")
+			lst.Append("ike")
+			lst.Append("jut")
+			lst.Append("kni")
+			lst.Append("lam")
+			lst.Append("mut")
+			lst.Append("nop")
+			lst.Append("orc")
+			//print
+			fmt.Println(lst)
+			g.Assert(lst.Len()).Equal(17)
+			g.Assert(lst.Len()).Equal(17)
+			g.Assert(lst.Pop()).Equal("orc")
+			g.Assert(lst.PopLeft()).Equal("foo")
+
+			g.Assert(lst.Len()).Equal(15)
+			g.Assert(lst.At(0)).Equal("bar")
+
+			g.Assert(lst.At(1)).Equal("baz")
+			g.Assert(lst.At(-0)).Equal("bar")
+			g.Assert(lst.At(-1)).Equal("nop")
+			g.Assert(lst.At(5)).Equal("elf")
+
+			g.Assert(lst.Len()).Equal(15)
+			lst.Clear()
+			g.Assert(lst.Len()).Equal(0)
+
+
+		})
+
+		g.It("should test list util funcs", func() {
+			var lst = NewList(1)
+			g.Assert(lst.Pop() == nil).IsTrue()
+			g.Assert(lst.PopLeft() == nil).IsTrue()
+			lst.AppendLeft("first")
+			lst.AppendLeft("second")
+			g.Assert(lst.First().(string)).Equal("second")
+			g.Assert(lst.PopLeft().(string)).Equal("second")
+			g.Assert(lst.PopLeft().(string)).Equal("first")
+			g.Assert(lst.First() == nil).IsTrue()
+			lst.AppendLeft("first")
+			lst.AppendLeft("second")
+			lst.Pop()
+			g.Assert(lst.First().(string)).Equal("second")
+			lst.Pop()
+			g.Assert(lst.Last() == nil).IsTrue()
+			g.Assert(lst.At(1) == nil).IsTrue()
+
+			lst.Append("foo")
+			lst.AppendLeft("bar")
+			lst.Append("baz")
+			var each_item = []string{"bar", "foo", "baz"}
+			lst.Each(func(o interface{}, i int) bool {
+				g.Assert(o.(string)).Equal(each_item[i])
+				return true
+			})
+			//head and tail should be the same
+			g.Assert(lst.Len()).Equal(3)
+			g.Assert(lst.First().(string)).Equal("bar")
+			g.Assert(lst.Last().(string)).Equal("baz")
+			g.Assert(lst.Pop().(string)).Equal("baz")
+			g.Assert(lst.PopLeft().(string)).Equal("bar")
+			g.Assert(lst.First().(string)).Equal("foo")
+			g.Assert(lst.Last().(string)).Equal("foo")
+			g.Assert(lst.PopLeft().(string)).Equal("foo")
+
+		})
+	})
+}
